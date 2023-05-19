40dd53af02b3355e938c071578034719f4dc6b52
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-05-19 17:42:36 +0700

    menambah sebuah Node kedalam list

--- Linked_List_OOP/Linked_List_OOP.cpp
+++ Linked_List_OOP/Linked_List_OOP.cpp
@@ -21,3 +21,54 @@ public:
 	bool delNode(int element);
 	void traverse();
 };
+
+List::List()
+{
+	START = NULL;
+}
+void List::addNode() //*Menambah sebuah Node kedalam list
+{
+	int nim;
+	char nm[20];
+	cout << "\nMasukkan Nomor Mahasiswa: ";
+	cin >> nim;
+	cout << "\nMasukkan Nama: ";
+	cin >> nm;
+
+	Node* nodeBaru = new Node;
+	nodeBaru->noMhs = nim;
+	strcpy_s(nodeBaru->nama, nm);
+
+	if (START == NULL || nim <= START->noMhs)
+	{
+		if ((START != NULL) && (nim == START->noMhs))
+		{
+			cout << "\nDuplikasi noMhs tidak diijinkan\n";
+			return;
+		}
+		nodeBaru->next == START;
+		START = nodeBaru;
+		return;
+	}
+	Node* previous, * current;
+	current = START;
+	previous = START; 
+
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "\nDuplikasi noMhs tidak diijinkan\n";
+			return;
+		}
+		previous = current;
+		current = current->next;
+
+
+	}
+	//*Jika loop diatas dieksekusi, previous dan current akan menempati posisi dimana
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
+
